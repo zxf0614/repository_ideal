c12691138bae7dc7af9094fd464662de5a104dc2
zxf0614 <1072520398@qq.com>
2018-12-18 19:46:06 +0800

    添加了address属性

--- git_demo01/src/com/itheima/domain/User.java
+++ git_demo01/src/com/itheima/domain/User.java
@@ -4,6 +4,8 @@ public class User {
     private String name;
     private int age;
 
+    private String address;
+
     public User(String name, int age) {
         this.name = name;
         this.age = age;
